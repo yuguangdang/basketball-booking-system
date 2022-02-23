0ae25236afb44fc63f4c928035593fa4224167d7
STREETBAOLANG <55920971+STREETBAOLANG@users.noreply.github.com>
2022-02-23 14:52:23 +0000

    2nd version

--- controllers/game.js
+++ controllers/game.js
@@ -3,6 +3,7 @@ const Player = require("../models/player");
 const getDate = require("../util/getDate");
 const constants = require("../lib/constants");
 const stripe = require("stripe")(process.env.STRIPE_KEY);
+const flash = require("connect-flash/lib/flash");
 
 exports.getPlayers = (req, res, next) => {
   req.game.populate("players.playerId").then((game) => {
@@ -22,7 +23,36 @@ exports.getPlayers = (req, res, next) => {
   });
 };
 
-exports.postAddPlayer = (req, res, next) => {
+exports.getJoinGame = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("game/join-game.ejs", {
+    path: "/join-game",
+    pageTitle: "Join Game",
+    name: req.session.player.name,
+    date: getDate(),
+    address: constants.ADDRESS,
+    gameFee: req.game.fee,
+    isAuthenticated: req.session.isLoggedIn,
+    time: constants.TIME,
+    errorMessage: message,
+  });
+};
+
+exports.postJoinGame = (req, res, next) => {
+  const playerIndex = req.game.players.findIndex((p) => {
+    return p.playerId.toString() === req.session.player._id.toString();
+  });
+
+  if (playerIndex >= 0 && req.session.player.email !== constants.ADMIN_EMAIL) {
+    req.flash("error", "You can only join the game once!");
+    return res.redirect("/join-game");
+  }
+
   req.game.addPlayer(req.session.player);
   Player.findOne({ email: req.session.player.email })
     .then((player) => {
@@ -44,19 +74,6 @@ exports.postCancel = (req, res, next) => {
       console.log(err);
     });
   res.redirect("/");
-};
-
-exports.getJoinGame = (req, res, next) => {
-  res.render("game/join-game.ejs", {
-    path: "/join-game",
-    pageTitle: "Join Game",
-    name: req.session.player.name,
-    date: getDate(),
-    address: constants.ADDRESS,
-    gameFee: req.game.fee,
-    isAuthenticated: req.session.isLoggedIn,
-    time: constants.TIME
-  });
 };
 
 exports.getCheckout = (req, res, next) => {
--- routes/game.js
+++ routes/game.js
@@ -8,15 +8,13 @@ router.get("/", playersController.getPlayers);
 
 router.get("/join-game", playersController.getJoinGame);
 
-router.post("/add-player", playersController.postAddPlayer);
+router.post("/join-game", playersController.postJoinGame);
 
 router.get("/checkout", playersController.getCheckout);
 
 router.get('/checkout/success', playersController.getCheckoutSuccess);
 
 router.get('/checkout/cancel', playersController.getCheckout);
-
-// router.post("/pay", playersController.postPay);
 
 router.post("/cancel", playersController.postCancel);
 
